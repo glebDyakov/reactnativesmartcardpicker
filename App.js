14210a2e6dc6880216d0bd85081c6aa606826432
glebDyakov <67467704+glebDyakov@users.noreply.github.com>
2021-09-02 16:17:16 +0300

    addnavigation

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button, Image, ScrollView, TouchableOpacity, AsyncStorageStatic } from 'react-native';
+
 // import { AsyncStorage } from '@react-native-community';
+// import {AsyncStorage} from '@react-native-async-storage/async-storage';
 
 // import { CardList } from './activities/CardList.js';
 
@@ -9,19 +11,26 @@ import * as SQLite from 'expo-sqlite';
 import { useState, useEffect } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-import * as Linking from 'expo-linking';
+import { createStackNavigator } from '@react-navigation/stack';
+// import * as Linking from 'expo-linking';
 
 
-const prefix = Linking.createURL('/');
+// const prefix = Linking.createURL('/');
 export default function App() {
-  const linking = {
-    prefixes: [prefix],
-  }
+  // const linking = {
+  //   prefixes: [prefix],
+  // }
+
+  const Stack = createStackNavigator();
+
   return (
-    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
-      <Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="CardsListActivity">
+        <Stack.Screen name="CardDetailActivity" component={CardDetailActivity} />
+        <Stack.Screen name="BindingActivity" component={BindingActivity} />
+        <Stack.Screen name="CardsTypeActivity" component={CardsTypeActivity} />
         <Stack.Screen name="CardsListActivity" component={CardsListActivity} />
+        <Stack.Screen name="LoginActivity" component={LoginActivity} />
         <Stack.Screen name="MainActivity" component={MainActivity} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -433,7 +442,7 @@ function MainActivity() {
 
 }
 
-function CardsListActivity() {
+function CardsListActivity({ navigation }) {
   
 
   const fiveImage = require('./assets/five.jpg')
@@ -522,11 +531,38 @@ function CardsListActivity() {
           cardsInDB.map(card => {
             return (
               Object.values(card)[3].includes("five") ?
-                <Image key={Object.values(card)[0]} style={{ marginTop: '15px', width: '650px', height: '250px' }} source={ fiveImage } />
+                <TouchableOpacity key={Object.values(card)[0]} onPress={() => {
+                  navigation.navigate('CardDetailActivity', {
+                    cardId: Object.values(card)[0],
+                    cardName: Object.values(card)[1],
+                    barCode: Object.values(card)[2],
+                    cardType: Object.values(card)[3],
+                  })
+                }}>
+                  <Image style={{ marginTop: '15px', width: '650px', height: '250px' }} source={ fiveImage } />
+                </TouchableOpacity>
                 : Object.values(card)[3].includes("cross") ?
-                  <Image key={Object.values(card)[0]} style={{ marginTop: '15px', width: '650px', height: '250px' }} source={ crossImage } />
+                  <TouchableOpacity key={Object.values(card)[0]} onPress={() => {
+                    navigation.navigate('CardDetailActivity', {
+                      cardId: Object.values(card)[0],
+                      cardName: Object.values(card)[1],
+                      barCode: Object.values(card)[2],
+                      cardType: Object.values(card)[3],
+                    })
+                  }}>
+                    <Image style={{ marginTop: '15px', width: '650px', height: '250px' }} source={ crossImage } />
+                  </TouchableOpacity>  
                 :
-                  <Image key={Object.values(card)[0]} style={{ marginTop: '15px', width: '650px', height: '250px' }} source={ magnetImage } />
+                <TouchableOpacity key={Object.values(card)[0]} onPress={() => {
+                    navigation.navigate('CardDetailActivity', {
+                    cardId: Object.values(card)[0],
+                    cardName: Object.values(card)[1],
+                    barCode: Object.values(card)[2],
+                    cardType: Object.values(card)[3],
+                  })
+                }}>
+                  <Image style={{ marginTop: '15px', width: '650px', height: '250px' }} source={ magnetImage } />
+                </TouchableOpacity>
             )
           })
         }
@@ -538,6 +574,7 @@ function CardsListActivity() {
             <Button color="rgb(255, 0, 0)" style={ styles.addCardBtn } title="+"
               onPress={ () => {
                 console.log("Перейти в окно выбора типа карты")
+                navigation.navigate('CardsTypeActivity')
               } }
             />
           </View>
@@ -608,3 +645,303 @@ const styles = StyleSheet.create({
     borderBottomWidth: '1px'
   }
 });
+
+function CardDetailActivity({ navigation, route }) {
+  
+  const fiveImage = require('./assets/five.jpg')
+  const crossImage = require('./assets/cross.jpg')
+  const magnetImage = require('./assets/magnet.jpg')
+  const barcodeImage = require('./assets/barcode.jpg')
+  const cameraImage = require('./assets/camera.png')
+  
+  const { cardId, cardName, barCode, cardType } = route.params
+
+  const state = {
+
+  }
+  
+  return (
+    <View>      
+      <View style={ styles.cardDetail }>
+        {
+          cardType.includes('five') ?
+            <Image style={{ width: '650px', height: '250px' }} source={ fiveImage } />
+          : cardType.includes('cross') ?
+            <Image style={{ width: '650px', height: '250px' }} source={ crossImage } />
+          :
+            <Image style={{ width: '650px', height: '250px' }} source={ magnetImage } />
+        }
+        <Text style={{
+          fontSize: '36px',
+          marginTop: '65px',
+          marginBottom: '65px',
+        }}>{ cardName }</Text>
+        <Image style={{ width: '250px', height: '100px' }} source={ barcodeImage } />
+        <Text>{ barCode }</Text>
+      </View>
+    </View>
+  );
+
+}
+
+function BindingActivity({ route, navigation }) {
+  const fiveImage = require('./assets/five.jpg')
+  const crossImage = require('./assets/cross.jpg')
+  const magnetImage = require('./assets/magnet.jpg')
+  const barcodeImage = require('./assets/barcode.jpg')
+  const cameraImage = require('./assets/camera.png')
+  const state = {
+    inputCardName: '',
+    inputBarCode: ''
+  }
+  
+  const { cardType } = route.params
+
+  const db = SQLite.openDatabase('smartcardspickerdb.db')
+  return (
+    <View>
+      <View style={ styles.bindingCard }>
+        {
+        cardType.includes('five') ?
+          <Image style={{ width: '650px', height: '250px' }} source={ fiveImage } />
+        : cardType.includes('cross') ?
+          <Image style={{ width: '650px', height: '250px' }} source={ crossImage } />
+        :
+          <Image style={{ width: '650px', height: '250px' }} source={ magnetImage } />
+        }
+        <Text>Введите название карты</Text>
+        <TextInput
+            style={styles.inputLabel}
+            onChangeText={(currentCardName) => {
+              state.inputCardName = currentCardName
+              console.log(`Введённое имя карты: ${state.inputCardName}`);
+            }}
+          />
+        <Text>Введите штрихкод</Text>
+        <View style={{ alignItems: 'center', flexDirection: 'row' }}>
+          <TextInput
+            style={styles.inputLabel}
+            onChangeText={(currentBarCode) => {
+              state.inputBarCode = currentBarCode
+              console.log(`Введённое штрих-код карты: ${state.inputBarCode}`);
+            }}
+          />
+            <TouchableOpacity onPress={() => {
+              console.log("открываем камеру")
+            }}>
+              <Image style={{ margin: '15px', width: '30px', height: '30px' }} source={ cameraImage } />
+            </TouchableOpacity>
+          </View>
+          <View style={ styles.addCardBtnLayout }>
+            <Button color="rgb(255, 0, 0)" style={ styles.addCardBtn } title="Прикрепить карту" 
+              onPress={ () => {
+                console.log("Добавить карту")
+                db.transaction(transaction => {
+                let sqlStatement = `INSERT INTO \"smartcards\"(cardname, barcode, cardtype) VALUES (\"${state.inputCardName}\", \"${state.inputBarCode}\", \"${cardType}\");`
+                  transaction.executeSql(sqlStatement, null, (tx, card) => {
+                    console.log("successOfInsert")
+                  }, (tx) => {
+                    console.log("errorOfInsert")
+                  })
+                })
+              } }
+            />
+          </View>
+      </View>
+    
+    </View>
+  );
+
+}
+
+function CardsTypeActivity({ navigation }) {
+  
+
+  const fiveImage = require('./assets/five.jpg')
+  const crossImage = require('./assets/cross.jpg')
+  const magnetImage = require('./assets/magnet.jpg')
+  const barcodeImage = require('./assets/barcode.jpg')
+  const cameraImage = require('./assets/camera.png')
+
+  return (
+    <View>
+      <View style={{
+        alignItems: 'center', flexDirection: 'column'
+      }}>
+        <ScrollView style={styles.scrollView}>
+          <TouchableOpacity onPress={() => {
+            console.log("переходим в окно добавления карты")
+            navigation.navigate('BindingActivity', {
+              cardType: "five"
+            })
+          }}>
+            <Image style={{ width: '650px', height: '250px' }} source={ fiveImage } />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            console.log("переходим в окно добавления карты")
+            navigation.navigate('BindingActivity', {
+              cardType: "cross"
+            })
+          }}>
+            <Image style={{ width: '650px', height: '250px' }} source={ crossImage } />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            console.log("переходим в окно добавления карты")
+            navigation.navigate('BindingActivity', {
+              cardType: "magnet"
+            })
+          }}>
+            <Image style={{ width: '650px', height: '250px' }} source={ magnetImage } />
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </View>
+  );
+
+}
+
+function LoginActivity() {
+  
+
+  const fiveImage = require('./assets/five.jpg')
+  const crossImage = require('./assets/cross.jpg')
+  const magnetImage = require('./assets/magnet.jpg')
+  const barcodeImage = require('./assets/barcode.jpg')
+  const cameraImage = require('./assets/camera.png')
+
+  const [ password, setPassword ] = useState('')
+  const state = {
+    password: ''
+  }
+
+  const db = SQLite.openDatabase('smartcardspickerdb.db')
+  const needPassword = true
+  db.transaction(transaction => {
+    let sqlStatement = "SELECT * FROM passwords;"
+    transaction.executeSql(sqlStatement, null, (tx, receivedPasswords) => {
+      if(receivedPasswords.rows.length >= 1){
+        needPassword = false
+      }
+    })
+  })
+  function checkPassword(num) {
+    console.log(`набираем пароль`)
+    if(password.length <= 3){
+      setPassword(password + num)
+      console.log(`текущий пароль: ${password}`)
+      if (password.length >= 4) {
+        if(needPassword){
+          console.log("Создаем пароль")
+          db.transaction(transaction => {
+            let sqlStatement = `INSERT INTO \"passwords\"(password) VALUES (\"${password}\");`
+            transaction.executeSql(sqlStatement, null, (tx, receivedPassword) => {
+              console.log("успешное создание пароля")
+            }, (tx) => {
+              console.log("ошибка создание пароля")
+              console.log("errorOfInsertPassword")
+            })
+
+          })
+        } else if(!needPassword){
+          console.log("пароль уже создан")
+          db.transaction(transaction => {
+            let sqlStatement = "CREATE TABLE IF NOT EXISTS passwords (_id INTEGER PRIMARY KEY AUTOINCREMENT, password TEXT);"
+            transaction.executeSql(sqlStatement, null, (tx, receivedTable) => {
+              console.log(`tablecreate: ${receivedTable}`)
+            })
+            sqlStatement = "SELECT * FROM passwords;"
+            transaction.executeSql(sqlStatement, null, (tx, receivedPasswords) => {
+              if(password.includes(receivedPasswords.rows.item(0)[1])){
+                console.log("пароль совпадают")
+                //загружаем окно списка прикреплённых карт
+              } else if(!password.includes(receivedPasswords.rows.item(0)[1])){
+                console.log("пароль не совпадают")
+                
+              }
+            })
+          })
+        }
+      }
+    }
+  }
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <View style={styles.numberRow}>
+          <Text style={styles.login}>Войти</Text>
+        </View>
+        <View style={styles.numberRow}>
+          <TextInput
+            secureTextEntry={true}
+            style={styles.password}
+            value={password}
+            onChangeText={(currentPassword) => {
+              state.password = currentPassword
+              console.log(`Введёный пароль: ${state.password}`);
+            }}
+          />
+        </View>
+        <View style={styles.numberRow}>
+          <TouchableOpacity onPress={() => {
+            checkPassword("1")
+          }}>
+            <Text style={styles.numberCell}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            checkPassword("2")
+          }}>
+          <Text style={styles.numberCell}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            checkPassword("3")
+          }}>
+          <Text style={styles.numberCell}>3</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.numberRow}>
+          <TouchableOpacity onPress={() => {
+            checkPassword("4")
+          }}>
+            <Text style={styles.numberCell}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            checkPassword("5")
+          }}>
+            <Text style={styles.numberCell}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            checkPassword("6")
+          }}>
+            <Text style={styles.numberCell}>6</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.numberRow}>
+          <TouchableOpacity onPress={() => {
+            checkPassword("7")
+          }}>
+            <Text style={styles.numberCell}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            checkPassword("8")
+          }}>
+            <Text style={styles.numberCell}>8</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {
+            checkPassword("9")
+          }}>
+            <Text style={styles.numberCell}>9</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.numberRow}>
+          <TouchableOpacity onPress={() => {
+            setPassword(password.slice(0, password.length - 1)) 
+          }}>
+            <Text style={styles.numberCell}>C</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+
+}
